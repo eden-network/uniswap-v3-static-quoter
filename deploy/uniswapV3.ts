c68cc6fbf8c9c7fe92ed8e2ed8ae4ca100e86d70
halo3mic <46010359+halo3mic@users.noreply.github.com>
2022-09-20 12:12:54 +0100

    redeploy univ3 quoter on eth,arb,op

--- deploy/uniswapV3.ts
+++ deploy/uniswapV3.ts
@@ -1,22 +1,21 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { abi as QUOTERV2_ABI } from "@uniswap/v3-periphery/artifacts/contracts/lens/QuoterV2.sol/QuoterV2.json";
+
+import addresses from "../test/addresses.json";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments, getNamedAccounts, ethers } = hre;
+    const { deployments, getNamedAccounts, network } = hre;
     const { deploy, log } = deployments;
 
+    
+    const allowedNetworks = ["optimism", "arbitrum", "mainnet"]
+    const contractName = "UniswapV3StaticQuoter";
+    const args = [ addresses.ethereum.protocols.uniswapV3.factory ]
     const { deployer } = await getNamedAccounts();
 
-    const quoterV2 = new ethers.Contract(
-        "0x61fFE014bA17989E743c5F6cB21bF9697530B21e",
-        QUOTERV2_ABI,
-        ethers.provider
-    );
-    const factory = await quoterV2.factory();
-    const weth = await quoterV2.WETH9();
 
-    const contractName = "UniswapV3StaticQuoter";
+    if (!allowedNetworks.includes(network.name))
+        throw new Error(`Wrong network! Only "${allowedNetworks}" supported`);
 
     log("1) Deploy contract");
     const deployResult: any = await deploy(contractName, {
@@ -24,16 +23,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         contract: contractName,
         skipIfAlreadyDeployed: true,
         log: true,
-        args: [factory, weth]
+        args
     });
 
-    if (deployResult.newlyDeployed) {
-        log(`- Factory: ${factory}`);
-        log(`- WETH9: ${weth}`);
-    }
+    if (deployResult.newlyDeployed)
+        log(`- 🎉 Deployed at: ${deployResult.address}`);   
     else
-        log(`- Deployment skipped, using previous deployment at: ${deployResult.address}`);
+        log(`- ⏩ Deployment skipped, using previous deployment at: ${deployResult.address}`);
 };
 
 export default func;
-func.tags = ["1", "Deploy"]
+func.tags = [ "uniswapV3" ]
